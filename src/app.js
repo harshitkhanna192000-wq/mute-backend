b3d9fa221a279b78defa98c72a9f04b0e90da668
anonymousk682-maker <anonymous682@gmail.com>
2026-01-19 21:21:10 +0530

    mke

--- src/app.js
+++ src/app.js
@@ -11,7 +11,14 @@ const messageRoutes = require("./modules/messages/message.routes");
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://muted-nine.vercel.app",
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    credentials: true
+  })
+);
+
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
